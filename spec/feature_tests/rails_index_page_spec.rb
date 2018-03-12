5a9b14c9f695017dcbe38524545d59e5e61b2596
kotpsot <zygmunt.justyna@gmail.com>
2018-03-12 16:46:44 +0000

    Add restaurant controller and index route

--- spec/feature_tests/rails_index_page_spec.rb
+++ spec/feature_tests/rails_index_page_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 feature 'index page' do
   scenario 'when index page loads' do
-    visit('localhost:3000')
-    expect(page).to have_content("Yay! You’re on Rails!")
+    visit('/')
+    expect(page).to have_content("HELLO!!!")
   end
 end
